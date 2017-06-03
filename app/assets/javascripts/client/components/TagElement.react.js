977d221b39a8c57366afcc5145d8b51db284c118
hiriorima <g2117001@fun.ac.jp>
2017-06-03 20:23:50 +0900

    Add TagList View

---
+++ app/assets/javascripts/client/components/TagElement.react.js
@@ -0,0 +1,26 @@
+import React from'react';
+
+import tagElement from'../templates/TagElement.jade';
+
+
+const TagElement = React.createClass({
+
+  render : tagElement,
+
+  componentWillMount : function(){
+  },
+
+  componentDidMount : function (){
+  },
+
+  componentWillUpdate : function(){
+  },
+
+  componentDidUpdate : function(){
+  },
+
+  componentWillUnmount : function(){
+  },
+});
+
+module.exports = TagElement;
